7c53adecc22df26616380c64a8d32b3d614bbbd9
Jairo Alan Castelan Fernandez <jairoalanc@outlook.com>
2024-03-11 11:50:54 -0600

    archivo grafica

---
+++ src/grafica.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import  pandas as pd
+import serial_asyncio
+import asyncio
+
+lora_band = "915000000"
+lora_networkid = "6"
+lora_address = "0"
+
+async def main():
+    reader, writer = await serial_asyncio.open_serial_connection(url='COM3', baudrate=115200)
+
+    writer.write(("AT+BAND="+lora_band+'\r\n').encode())
+    await writer.drain()
+    writer.write(("AT+NETWORKID="+lora_networkid+'\r\n').encode())
+    await writer.drain()
+    writer.write(("AT+ADDRESS="+lora_address+'\r\n').encode())
+    await writer.drain()
+    writer.write(("AT+PARAMETER=7,7,1,4\r\n").encode())
+    await writer.drain()
+
+    while True:
+        data = await reader.read(100)
+        if data:
+            print(data.decode())
+
+asyncio.run(main())
